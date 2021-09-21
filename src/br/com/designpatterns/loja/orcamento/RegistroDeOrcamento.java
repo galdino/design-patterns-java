2c9d5acd2f9f3418e22b15528a71e2f894e7b655
Fernando Galdino <fernando.galdino@meta.com.br>
2021-09-21 15:53:05 -0300

    API for budget register

---
+++ src/br/com/designpatterns/loja/orcamento/RegistroDeOrcamento.java
@@ -0,0 +1,12 @@
+package br.com.designpatterns.loja.orcamento;
+
+public class RegistroDeOrcamento {
+	
+	public void registrar(Orcamento orcamento) {
+		// chamada HTTP para API externa
+		// URL Connection
+		// Http Client
+		// lib REST
+	}
+
+}
