6452eaed35b9c6a8d361cde1598a64939bbb695f
Fernando Galdino <fernando.galdino@meta.com.br>
2021-09-22 15:33:41 -0300

    Implementing Decorator Pattern

--- src/br/com/designpatterns/loja/TestesImpostos.java
+++ src/br/com/designpatterns/loja/TestesImpostos.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import br.com.designpatterns.loja.imposto.CalculadoraDeImpostos;
 import br.com.designpatterns.loja.imposto.ICMS;
+import br.com.designpatterns.loja.imposto.ISS;
 import br.com.designpatterns.loja.orcamento.Orcamento;
 
 public class TestesImpostos {
@@ -11,7 +12,7 @@ public class TestesImpostos {
 	public static void main(String[] args) {
 		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
 		CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
-		System.out.println(calculadoraDeImpostos.calcular(orcamento, new ICMS()));
+		System.out.println(calculadoraDeImpostos.calcular(orcamento, new ICMS(new ISS(null))));
 	}
 
 }
--- src/br/com/designpatterns/loja/imposto/Imposto.java
+++ src/br/com/designpatterns/loja/imposto/Imposto.java
@@ -4,8 +4,25 @@ import java.math.BigDecimal;
 
 import br.com.designpatterns.loja.orcamento.Orcamento;
 
-public interface Imposto {
+public abstract class Imposto {
 	
-	BigDecimal calcular(Orcamento orcamento);
+	private Imposto impostoAux;
+	
+	public Imposto(Imposto impostoAux) {
+		this.impostoAux = impostoAux;
+	}
+
+	protected abstract BigDecimal realizarCalculo(Orcamento orcamento);
+	
+	public BigDecimal calcular(Orcamento orcamento) {
+		BigDecimal valorImposto = this.realizarCalculo(orcamento);
+		BigDecimal valorImpostoAux = BigDecimal.ZERO;
+		
+		if(this.impostoAux != null) {
+			valorImpostoAux = this.impostoAux.calcular(orcamento);
+		}
+		
+		return valorImposto.add(valorImpostoAux);
+	}
 
 }
