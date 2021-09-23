5a4fc83ee2e138afec0259673aa0d4e322b64c28
Fernando Galdino <fernando.galdino@meta.com.br>
2021-09-23 14:51:57 -0300

    The process of creating a order

--- src/br/com/designpatterns/loja/TestesPedidos.java
+++ src/br/com/designpatterns/loja/TestesPedidos.java
@@ -8,6 +8,7 @@ import br.com.designpatterns.loja.orcamento.Orcamento;
 import br.com.designpatterns.loja.pedido.GeraPedido;
 import br.com.designpatterns.loja.pedido.GeraPedidoHandler;
 import br.com.designpatterns.loja.pedido.acao.EnviarEmailPedido;
+import br.com.designpatterns.loja.pedido.acao.LogDePedido;
 import br.com.designpatterns.loja.pedido.acao.SalvarPedido;
 
 public class TestesPedidos {
@@ -20,7 +21,7 @@ public class TestesPedidos {
 		orcamento.adicionarItem(new ItemOrcamento(new BigDecimal("150")));
 		
 		GeraPedido geraPedido = new GeraPedido(cliente, orcamento);
-		GeraPedidoHandler geraPedidoHandler = new GeraPedidoHandler(Arrays.asList(new SalvarPedido(), new EnviarEmailPedido()));
+		GeraPedidoHandler geraPedidoHandler = new GeraPedidoHandler(Arrays.asList(new SalvarPedido(), new EnviarEmailPedido(), new LogDePedido()));
 		geraPedidoHandler.execute(geraPedido);
 	}
 
