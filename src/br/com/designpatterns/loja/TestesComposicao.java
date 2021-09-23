2b5f70ebb964b55b7a95c9b467729148c257efc0
Fernando Galdino <fernando.galdino@meta.com.br>
2021-09-23 16:03:12 -0300

    Implementing Proxy Pattern

--- src/br/com/designpatterns/loja/TestesComposicao.java
+++ src/br/com/designpatterns/loja/TestesComposicao.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import br.com.designpatterns.loja.orcamento.ItemOrcamento;
 import br.com.designpatterns.loja.orcamento.Orcamento;
+import br.com.designpatterns.loja.orcamento.OrcamentoProxy;
 
 public class TestesComposicao {
 	
@@ -16,7 +17,10 @@ public class TestesComposicao {
 		novo.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
 		novo.adicionarItem(antigo);
 		
-		System.out.println(novo.getValor());
+		OrcamentoProxy orcamentoProxy = new OrcamentoProxy(novo);
+		
+		System.out.println(orcamentoProxy.getValor());
+		System.out.println(orcamentoProxy.getValor());
 	}
 
 }
